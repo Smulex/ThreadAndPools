d92a88dcddd8455c90a998ea6b1a80e48c3139c7
Christian Bøgh <chri45i4@zbc.dk>
2020-12-14 13:32:48 +0100

    4

---
+++ Øvelse4/Øvelse4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Øvelse4
+{
+    class InputOutput
+    {
+        char input = '*';
+        public void Printer()
+        {
+            while (true)
+            {
+                Console.Write(input);
+                Thread.Sleep(1);
+            }
+        }
+
+        public void Reader()
+        {
+            while (true)
+            {
+                try
+                {
+                    char readCharacter = Convert.ToChar(Console.ReadLine());
+                    input = readCharacter;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+        }
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            InputOutput inputOutput = new InputOutput();
+
+            Thread threadPrinter = new Thread(new ThreadStart(inputOutput.Printer));
+            threadPrinter.Start();
+
+            Thread threadReader = new Thread(new ThreadStart(inputOutput.Reader));
+            threadReader.Start();
+
+            threadReader.Join();
+            Console.ReadKey();
+        }
+    }
+}
