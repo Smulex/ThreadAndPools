eb6359dbf8c4b966c003692476b281826fc205cb
Christian Bøgh <chri45i4@zbc.dk>
2020-12-14 13:21:41 +0100

    3

---
+++ Øvelse 3/Øvelse 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermometer thermometer = new Thermometer();
+            Thread thermometerThread = new Thread(new ThreadStart(thermometer.Alarm));
+            thermometerThread.Start();
+            while (true)
+            {
+                if (!thermometerThread.IsAlive)
+                {
+                    Console.WriteLine("Alarm-tråd termineret!");
+                    break;
+                }
+                Thread.Sleep(10000);
+            }
+        }
+    }
+    class Thermometer
+    {
+
+        public void Alarm()
+        {
+            int alarmCounter = 0;
+            while (alarmCounter < 3)
+            {
+                int temperature = RandomNumber(-20, 120);
+                Console.WriteLine("Temprature = " + temperature);
+                if (temperature <= 0 || temperature >= 100)
+                {
+                    Console.WriteLine("Alarm");
+                    alarmCounter++;
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        public int RandomNumber(int startNumbe, int endNumber)
+        {
+            Random generator = new Random();
+            int temperature = generator.Next(startNumbe, endNumber);
+            return temperature;
+        }
+    }
+}
