fa174499c90d87bef3b8cc6272e0482e5c71493f
Christian Bøgh <chri45i4@zbc.dk>
2020-12-14 13:50:42 +0100

    3

---
+++ Pool/Pool3/Pool3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Pool3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start() - Starter en tråd
+            //Sleep() - Pauser en tråd 
+            //Suspend() - Stopper tråd of venter på at blive resumed
+            //Resume() - Genoptager en tråd
+            //Abort() - Fjerner tråden
+            //Join() - Venter må alle tråde i mainer færdige før den fortsætter.
+
+            Thread thread1 = Thread.CurrentThread;
+            thread1.Priority = ThreadPriority.Highest;
+            ThreadPool.QueueUserWorkItem(MyMethod, thread1);
+
+            Thread thread2 = Thread.CurrentThread;
+            thread2.IsBackground = true;
+            ThreadPool.QueueUserWorkItem(MyMethod, thread2);
+
+            Console.ReadKey();
+        }
+
+        public static void MyMethod(object obj)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread thread = (Thread)obj;
+                string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}, Thread Priority: {thread.Priority}";
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
