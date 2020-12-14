5992e723a991ee1b928efdbd00d62a3bbf01929f
Christian Bøgh <chri45i4@zbc.dk>
2020-12-14 13:45:32 +0100

    2

--- Pool/Pool2/Pool2/Program.cs
+++ Pool/Pool2/Pool2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Pool2
 {
@@ -10,6 +12,40 @@ namespace Pool2
     {
         static void Main(string[] args)
         {
+            Stopwatch mywatch = new Stopwatch();
+            Console.WriteLine("Thread Pool Execution");
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+            Console.WriteLine("Thread Execution");
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+            Console.ReadKey();
         }
+
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+        static void Process(object callback)
+        {
+        }
+
     }
 }
